cc21487a293f7ca864a6c114e714982a3f683a17
acecic82 <102938al@naver.com>
2024-09-05 02:39:59 +0900

    Upload File and Pages add

---
+++ pages/01_DocumentGPT.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
+
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
+
+if file:
+    st.write(file)
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    st.write(file_content, file_path)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
