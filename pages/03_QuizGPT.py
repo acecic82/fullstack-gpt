f60852a2584ccc97aeacf10687148f43103c42a9
acecic82 <102938al@naver.com>
2024-09-06 16:40:39 +0900

    start quizGPT and add basic sidebar

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,64 @@
 import streamlit as st
 
+from langchain.retrievers import WikipediaRetriever
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+from pathlib import Path
+
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓",
+)
+
 st.title("QuizGPT")
+
+QUIZ_DIR = "./.cache/quiz_files/"
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"{QUIZ_DIR}{file.name}"
+
+    try:
+        with open(file_path, "wb") as f:
+            f.write(file_content)
+    except FileNotFoundError:
+        Path(QUIZ_DIR).mkdir(parents=True, exist_ok=True)
+        with open(file_path, "wb") as f:
+            f.write(file_content)
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching WIkipedia..."):
+                docs = retriever.get_relevant_documents(topic)
+                st.write(docs)
